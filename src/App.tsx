703303627eb15a20ba7e89c1c62e0457e4eebb07
Julian Vogel <birdman.coding.1991@gmail.com>
2023-04-05 10:15:36 +0200

    stateAdv

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
+import React from "react";
 import Navigation from "./components/shared/navigation/Navigation";
 import Counter from "./components/home/Counter";
 import Sidebar from "./components/shared/navigation/Sidebar";
 import { useState } from "react";
+import Button from "./components/shared/Button";
 
 function App() {
   const [showSidebar, setShowSidebar] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({
+    firstName: "Julian",
+    lastName: "Vogel",
+  });
+
+  function sidebarHandler(e: React.MouseEvent, testVal: string) {
+    console.log(e.target, testVal);
+    setShowSidebar(true);
+  }
 
   return (
     <div id="wrapper">
-      <Navigation showSideBar={() => setShowSidebar(true)} />
+      <Navigation showSideBar={sidebarHandler} />
       <Sidebar isVisible={showSidebar} />
       <Counter />
+      <div>
+        <h2 className="text-2xl font-bold">{user.firstName}</h2>
+        <h2 className="text-2xl font-bold">{user.lastName}</h2>
+        <Button
+          variant="primary"
+          onClick={() => setUser((prev) => ({ ...prev, lastName: "maria" }))}
+        >
+          Change Last Name
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/shared/navigation/Navigation.tsx
+++ src/components/shared/navigation/Navigation.tsx
@@ -2,15 +2,16 @@ import { FaBars } from "react-icons/fa";
 import NavList from "./NavList";
 
 interface Props {
-  showSideBar: () => void;
+  showSideBar: (e: React.MouseEvent, test: string) => void;
 }
 
 function Navigation({ showSideBar }: Props) {
+  const test = "Hai Im Child Element";
   return (
     <nav className="bg-blue-400 shadow-md p-6 flex text-white justify-between items-center">
       <h3 className="text-lg font-bold cursor-pointer">My first Website</h3>
       <FaBars
-        onClick={() => showSideBar()}
+        onClick={(e) => showSideBar(e, test)}
         className="block md:hidden cursor-pointer"
       />
       <NavList className="gap-3 text-white hidden md:flex" />
